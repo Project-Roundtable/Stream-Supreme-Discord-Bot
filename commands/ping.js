e44d68a8a63ae2f1204ab8af8afe3c436272e1d7
Luke S <hyperconix@pm.me>
2023-04-22 00:52:18 +0100

    feat(command): add example commands
    Adding example commands from Discord.js for testing. These will be removed later.

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,10 @@
-//sample command file
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with Pong!"),
+    async execute(interaction) {
+        await interaction.reply("Pong!");
+    },
+};
