f0cc4b95d348bd211f5b1b195ac58550711677a6
Luke S <hyperconix@pm.me>
2023-04-25 07:08:08 +0100

    feat(main): add listener for commands

--- index.js
+++ index.js
@@ -63,5 +63,28 @@ client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+// Listener for commands
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
+        } else {
+            await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+        }
+    }
+});
+
 // Log in to Discord with your client's token
 client.login(BOT_TOKEN);
