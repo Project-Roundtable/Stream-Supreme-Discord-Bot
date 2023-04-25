e2304051b5bb8f47d7fb2fa39eb3428e7a14c88b
Luke S <hyperconix@pm.me>
2023-04-25 07:11:57 +0100

    feat(main): add subdirectories for commands and relevant handling

--- index.js
+++ index.js
@@ -34,27 +34,31 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
 /**
- * The path to the commands directory where slash commands will be loaded from.
+ * The path to the command folders and their subdirectories.
  */
-const commandsPath = path.join(__dirname, "commands");
+const foldersPath = path.join(__dirname, "commands");
 
 /**
- * The command files that have been loaded from the commands directory. (Only includes .js files)
+ * The command folders at the commands directory path. 
  */
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync(foldersPath);
 
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
 
-    const command = require(filePath);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
 
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+
+        const command = require(filePath);
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ("data" in command && "execute" in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
     }
-
 }
 
 // When the client is ready, run this code (only once)
